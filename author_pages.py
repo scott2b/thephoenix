d4ab7cf470ebf3c24da5b0cc5fb42537515f8668
Scott Bradley <scott.b.bradley@gmail.com>
2013-03-15 09:03:56 -0500

    Adds option to pass in city on command line. Defaults to Boston

--- author_pages.py
+++ author_pages.py
@@ -1,7 +1,31 @@
+"""
+Scrape author articles from thephoenix.com
+
+Currenly focused on Boston, although seems to generally be working for the
+others. Pass in city before author slug -- defaults to Boston.
+
+There might be a better way: word is that there is a private API.
+
+Usage:
+python author_pages.py [city] author-slug
+"""
 import lxml.html
 import sys
 import urllib2
 
+SITE_ROOTS = {
+    'boston': 'http://thephoenix.com/boston',
+    'portland': 'http://portland.thephoenix.com',
+    'providence': 'http://providence.thephoenix.com'
+}
+
+AUTHOR_URL = '%(site_root)s/authors/%(author_slug)s/'
+
+def prepend_link(city, link):
+    assert link.startswith('/'), 'Unsupported relative link'
+    if link.lower().startswith('/boston/'):
+        link = link[len('/boston'):]
+    return '%s%s' % (SITE_ROOTS[city], link)
 
 def fetch_page(url):
     page = urllib2.urlopen(url).read()
@@ -10,21 +34,31 @@ def fetch_page(url):
     print '\n\n'.join([e.text_content() for e in article])
 
 
-def fetch_author_articles(author):
-    url = 'http://thephoenix.com/boston/authors/%s/' % author
+def fetch_author_articles(city, author):
+    url = AUTHOR_URL % {
+        'site_root': SITE_ROOTS[city],
+        'author_slug': author
+    }
     page = urllib2.urlopen(url).read()
     doc = lxml.html.fromstring(page)
     links = doc.cssselect('div#ArticleList h3 a')
     for link in links:
         href = link.get('href')
         if not href.startswith('http:'):
-            href = 'http://thephoenix.com%s' % href
+            href = prepend_link(city, href)
+        print 'fetching', href
         fetch_page(href)
 
 
 if __name__=="__main__":
-    try:
-        author = sys.argv[1]
-        fetch_author_articles(author) 
-    except IndexError:
+    city = 'boston'
+    if len(sys.argv) == 1:
         print '\nPlease indicate an author\n'
+        sys.exit(0)
+    elif len(sys.argv) == 2:
+        city = 'boston'
+        author = sys.argv[1]
+    else:
+        city = sys.argv[1].lower()
+        author = sys.argv[2]
+    fetch_author_articles(city, author) 
